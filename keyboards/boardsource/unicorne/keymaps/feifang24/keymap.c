836df4d60432275bf2d6cd40ad3efa3c430cc21c
feifang24 <ffang19@stanford.edu>
2024-05-25 16:25:36 -0700

    use different code

--- keyboards/boardsource/unicorne/keymaps/feifang24/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/feifang24/keymap.c
@@ -27,9 +27,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT_split
                                                                   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
                                                                   KC_LCTL, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_VOLU,
                                                                   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-                                                                  KC_LALT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_UP, KC_L, KC_VOLD,
+                                                                  KC_LOPT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_UP, KC_L, KC_VOLD,
                                                                   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-                                                                  KC_LGUI, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY,
+                                                                  KC_LCMD, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY,
                                                                   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                                                   KC_ESC, KC_CAPS, KC_LSFT, KC_SPC, KC_BSPC, KC_DEL
                                                                   //`--------------------------'  `--------------------------'
